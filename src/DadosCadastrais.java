8d1ecbe5b6a138d173a158ba956680301670478e
Tuan-Simonetti <tuan.gomes@picpay.com>
2020-07-31 23:15:45 -0300

    Exercicios

---
+++ src/DadosCadastrais.java
@@ -0,0 +1,15 @@
+import java.util.Date;
+
+public class DadosCadastrais {
+
+    public static void main(String[] args) {
+
+        String celular = "(27) 99999-9999";
+        String nascimento = String.format("%d/%d/%d", 26, 9, 1994);
+        String dataCadastro = String.format("%d/%d/%d", 14, 1, 2020);
+        String email = "brasil@gmail.com";
+        String mgm = "VALOAB1";
+        String versao = "10.18.2";
+        Date ultimaVezOnline = new Date();
+   }
+}
---
+++ src/Estrato.java
@@ -0,0 +1,11 @@
+public class Estrato {
+    public static void main(String[] args) {
+
+        String data = String.format("%d/%d/%d", 13, 5, 2020);
+        String origin = "456123789";
+        String descricao = "saldo";
+        Double valor = 0.01;
+        Double saldo = 9.83;
+        Double retiravel = 0.01;
+    }
+}
